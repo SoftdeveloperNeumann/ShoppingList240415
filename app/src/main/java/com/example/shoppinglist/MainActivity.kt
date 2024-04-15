cf0d7d2d2954ab4639243d9fe31b37e654604aa2
Frank <f.neumann.berlin@googlemail.com>
2024-04-15 12:32:47 +0200

    DB wird erstellt und open und close implementiert

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.shoppinglist
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.shoppinglist.databinding.ActivityMainBinding
 
@@ -18,13 +19,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         datasource = ShoppingMemoDatasource(this)
+        initShoppingMemoListView()
 
+    }
+
+    private fun initShoppingMemoListView(){
+        val emptyListForInit: List<ShoppingMemo> = ArrayList()
+        val adapter = object : ArrayAdapter<ShoppingMemo>(this,
+            android.R.layout.simple_list_item_multiple_choice,
+            emptyListForInit){}
+
+        binding.lvShoppingMemos.adapter = adapter
+    }
+    
+    private fun showAllShoppingMemos(){
+        val list = datasource.allShoppingMemos
+        val adapter = binding.lvShoppingMemos.adapter as ArrayAdapter<ShoppingMemo>
+        adapter.clear()
+        adapter.addAll(list)
+        adapter.notifyDataSetChanged()
     }
 
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart: DataSource wird geöffnet")
         datasource.open()
+        showAllShoppingMemos()
     }
 
     override fun onStop() {
--- app/src/main/java/com/example/shoppinglist/ShoppingMemoDatasource.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingMemoDatasource.kt
@@ -1,26 +1,67 @@
 package com.example.shoppinglist
 
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.util.Log
 
 class ShoppingMemoDatasource(context: Context) {
 
-    private var db : SQLiteDatabase? = null
+    private var db: SQLiteDatabase? = null
     private val helper: ShoppingMemoDbHelper
 
     private val TAG = "ShoppingMemoDatasource"
+
+    private val columns = arrayOf(
+        ShoppingMemoDbHelper.COLUMN_ID,
+        ShoppingMemoDbHelper.COLUMN_QUANTITY,
+        ShoppingMemoDbHelper.COLUMN_PRODUCT
+    )
+
+    val allShoppingMemos: List<ShoppingMemo>
+        get() {
+            val shoppingMemoList: MutableList<ShoppingMemo> = ArrayList()
+
+            val cursor = db?.query(
+                ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,
+                columns,
+                null, null, null, null, null
+            )
+
+            cursor?.moveToFirst()
+            var memo: ShoppingMemo
+            while (!cursor?.isAfterLast!!) {
+                memo = cursorToShoppingMemo(cursor)
+                shoppingMemoList.add(memo)
+                cursor.moveToNext()
+            }
+            cursor.close()
+
+            return shoppingMemoList
+        }
+
+    private fun cursorToShoppingMemo(cursor: Cursor): ShoppingMemo {
+        val idIndex = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_ID)
+        val quantityIndex = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_QUANTITY)
+        val productIndex = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_PRODUCT)
+
+        val id = cursor.getLong(idIndex)
+        val quantity = cursor.getInt(quantityIndex)
+        val product = cursor.getString(productIndex)
+
+        return ShoppingMemo(quantity,product,id)
+    }
 
     init {
         helper = ShoppingMemoDbHelper(context)
         Log.d(TAG, "DB-Init: Datasource hat den Helper angelegt")
     }
 
-    fun open(){
+    fun open() {
         db = helper.writableDatabase
     }
 
-    fun close(){
+    fun close() {
         helper.close()
     }
 }
