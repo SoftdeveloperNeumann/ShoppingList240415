6ea5963646948d7d5dbe86f1d1305542fc9b8e7a
Frank <f.neumann.berlin@googlemail.com>
2024-04-16 12:06:46 +0200

    mit Markierung als Erledigt

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.shoppinglist
 
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.shoppinglist.databinding.ActivityMainBinding
 
@@ -15,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         datasource = ShoppingMemoDatasource(this)
@@ -25,13 +30,36 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun initShoppingMemoListView(){
+    private fun initShoppingMemoListView() {
         val emptyListForInit: List<ShoppingMemo> = ArrayList()
-        val adapter = object : ArrayAdapter<ShoppingMemo>(this,
+        val adapter = object : ArrayAdapter<ShoppingMemo>(
+            this,
             android.R.layout.simple_list_item_multiple_choice,
-            emptyListForInit){}
+            emptyListForInit
+        ) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view = super.getView(position, convertView, parent) as TextView
+                val memo = binding.lvShoppingMemos.getItemAtPosition(position) as ShoppingMemo
+
+                if(memo.isSelected){
+                    view.paintFlags = view.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                    view.setTextColor(Color.rgb(175,175,175))
+                }else{
+                    view.paintFlags = view.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+                    view.setTextColor(Color.DKGRAY)
+                }
+
+                return view
+            }
+        }
 
         binding.lvShoppingMemos.adapter = adapter
+
+        binding.lvShoppingMemos.setOnItemClickListener { parent, view, position, id ->
+            val memo = parent.getItemAtPosition(position) as ShoppingMemo
+            datasource.updateShoppingMemo(memo.quantity, memo.product, memo.id, !memo.isSelected)
+            showAllShoppingMemos()
+        }
     }
 
     private fun activateAddButton() {
@@ -39,13 +67,13 @@ class MainActivity : AppCompatActivity() {
             val quantityString = binding.etQuantity.text.toString()
             val product = binding.etProduct.text.toString()
 
-            if(quantityString.isBlank()){
+            if (quantityString.isBlank()) {
                 binding.etQuantity.error = "Anzahl darf nicht leer sein"
                 binding.etQuantity.requestFocus()
                 return@setOnClickListener
             }
 
-            if(product.isBlank()){
+            if (product.isBlank()) {
                 binding.etProduct.error = "Artikel darf nicht leer sein"
                 binding.etProduct.requestFocus()
                 return@setOnClickListener
@@ -55,14 +83,14 @@ class MainActivity : AppCompatActivity() {
             binding.etQuantity.text.clear()
             binding.etProduct.text.clear()
             binding.etQuantity.requestFocus()
-            datasource.createShoppingMemo(quantity,product)
+            datasource.createShoppingMemo(quantity, product)
             showAllShoppingMemos()
 
         }
     }
 
 
-    private fun showAllShoppingMemos(){
+    private fun showAllShoppingMemos() {
         val list = datasource.allShoppingMemos
         val adapter = binding.lvShoppingMemos.adapter as ArrayAdapter<ShoppingMemo>
         adapter.clear()
