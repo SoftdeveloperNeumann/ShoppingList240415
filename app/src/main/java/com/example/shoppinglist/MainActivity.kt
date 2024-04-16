6a7d769c01d46bcd65484465af059437145bf34c
Frank <f.neumann.berlin@googlemail.com>
2024-04-16 10:32:07 +0200

    mit Insert in der Datasource und der MainActivity

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -20,8 +20,10 @@ class MainActivity : AppCompatActivity() {
 
         datasource = ShoppingMemoDatasource(this)
         initShoppingMemoListView()
+        activateAddButton()
 
     }
+
 
     private fun initShoppingMemoListView(){
         val emptyListForInit: List<ShoppingMemo> = ArrayList()
@@ -31,7 +33,35 @@ class MainActivity : AppCompatActivity() {
 
         binding.lvShoppingMemos.adapter = adapter
     }
-    
+
+    private fun activateAddButton() {
+        binding.btnAddProduct.setOnClickListener {
+            val quantityString = binding.etQuantity.text.toString()
+            val product = binding.etProduct.text.toString()
+
+            if(quantityString.isBlank()){
+                binding.etQuantity.error = "Anzahl darf nicht leer sein"
+                binding.etQuantity.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(product.isBlank()){
+                binding.etProduct.error = "Artikel darf nicht leer sein"
+                binding.etProduct.requestFocus()
+                return@setOnClickListener
+            }
+
+            val quantity = quantityString.toInt()
+            binding.etQuantity.text.clear()
+            binding.etProduct.text.clear()
+            binding.etQuantity.requestFocus()
+            datasource.createShoppingMemo(quantity,product)
+            showAllShoppingMemos()
+
+        }
+    }
+
+
     private fun showAllShoppingMemos(){
         val list = datasource.allShoppingMemos
         val adapter = binding.lvShoppingMemos.adapter as ArrayAdapter<ShoppingMemo>
